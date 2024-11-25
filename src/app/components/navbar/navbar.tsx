d38195ba4fc080469a5610c6c85b3ba998fe8b14
Saina Firooz <saina.firooz@gmail.com>
2024-11-25 15:48:16 +0100

    session-based sign-out functionality in Navbar component

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,9 +1,16 @@
 import { BookImage } from "lucide-react";
 import Link from "next/link";
 import React from "react";
-import { buttonVariants } from "../../../components/ui/button";
+import { Button, buttonVariants } from "../../../components/ui/button";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   return (
     <div className="border-b px-4">
       <div className="flex items-center justify-between mx-auto max-w-4xl h-16">
@@ -13,9 +20,23 @@ export default function Navbar() {
         </Link>
 
         <div>
-          <Link href="/sign-in" className={buttonVariants()}>
-            Sign In
-          </Link>
+          {session ? (
+            <form
+              action={async () => {
+                "use server";
+                await auth.api.signOut({
+                  headers: await headers(),
+                });
+                redirect("/");
+              }}
+            >
+              <Button type="submit">Sign Out</Button>
+            </form>
+          ) : (
+            <Link href="/sign-in" className={buttonVariants()}>
+              Sign In
+            </Link>
+          )}
         </div>
       </div>
     </div>
