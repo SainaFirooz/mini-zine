534e673d56a94422eb15a2a3dd946f082a54c059
Karl Dollmayer <karl.dollmayer@daresay.co>
2024-12-10 11:53:03 +0100

    template card

--- src/app/testcardpage/page.tsx
+++ src/app/testcardpage/page.tsx
@@ -4,7 +4,26 @@ import React from "react";
 type Props = {};
 
 function page({}: Props) {
-  return <TestCard />;
+  return (
+    <div className="flex flex-row w-full gap-2 flex-wrap columns-8">
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+      <TestCard />
+    </div>
+  );
 }
 
 export default page;
--- src/components/TestCard.tsx
+++ src/components/TestCard.tsx
@@ -1,24 +1,30 @@
 "use client";
+import Image from "next/image";
 import React, { useState } from "react";
 
 type Props = {};
 
 function TestCard({}: Props) {
   const [text, setText] = useState<String>("");
+  const [image, setImage] = useState();
 
   function onChangeText(e: React.ChangeEvent<HTMLInputElement>) {
     setText(e.target.value);
   }
+  function onChangeImage(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log(e.target.files);
+  }
 
   return (
     <div className="bg-cyan-400 w-60 h-96">
-      <input type="file" accept="image/*" />
+      <input type="file" accept="image/*" onChange={(e) => onChangeImage(e)} />
       <input
         type="text"
         onChange={(e) => onChangeText(e)}
         placeholder="Write something here..."
       />
       <p>{text}</p>
+      <Image src={""} alt={"IMAGE"} />
     </div>
   );
 }
