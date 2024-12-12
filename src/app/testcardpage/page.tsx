0492da6aab32af739e3a838ea197c26b272cdfef
Saina Firooz <saina.firooz@gmail.com>
2024-12-12 10:01:42 +0100

    test card first version

--- src/app/testcardpage/page.tsx
+++ src/app/testcardpage/page.tsx
@@ -6,22 +6,15 @@ type Props = {};
 function page({}: Props) {
   return (
     <div className="flex flex-row w-full gap-2 flex-wrap columns-8">
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
-      <TestCard />
+      <TestCard  rotateImage={true} cardName="Page 4" style={{ transform: 'rotate(3.142rad)' }}/>
+      <TestCard  rotateImage={true} cardName="Page 3" style={{ transform: 'rotate(3.142rad)' }}/>
+      <TestCard  rotateImage={true} cardName="Page 2" style={{ transform: 'rotate(3.142rad)' }}/>
+      <TestCard  rotateImage={true} cardName="Page 1" style={{ transform: 'rotate(3.142rad)' }}/>
+      <TestCard cardName="Page 5"/>
+      <TestCard cardName="Page 6"/>
+      <TestCard cardName="Back Cover"/>
+      <TestCard cardName="Front Cover"/>
+    
     </div>
   );
 }
