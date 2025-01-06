cf27e2de4f380d3cb4b7772512899751fac514d7
Saina Firooz <saina.firooz@gmail.com>
2025-01-06 16:58:31 +0100

    changed texts in the hero section

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -24,14 +24,14 @@ export const Hero = () => {
       <div className="container">
         <div className="md:flex items-center">
           <div className="md:w-[478px]">
-            <div className="tag">Version 2.0 is here</div>
+            {/* <div className="tag">Version 2.0 is here</div> */}
             <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
-              Pathway to productivity
+              Create your own Mini Zine 
             </h1>
             <p className="text-xl text-[£010D3E] tracking-tight mt-6">
-              Celebrate the joy of accomplishment with an app designed to track
-              your progress, motivate your efforts, and celebrate your
-              successes.
+              Discover the joy of creating Mini Zines – small books made from a
+              single A4 sheet of paper. With our easy-to-use tools, you can quickly design and print your own Mini Zines using
+              personal photographs and text.
             </p>
             <div className="flex gap-1 items-center mt-[30px]">
               <button className="btn btn-primary">Sign up</button>
