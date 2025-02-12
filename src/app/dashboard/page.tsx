0d0d5fa570b173d80620f70b7a522af8fe75e8d7
Saina Firooz <saina.firooz@gmail.com>
2025-02-12 20:10:28 +0100

    changed button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,14 +13,15 @@ export default async function Dashboard() {
 
   return (
     <div className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
-    
-      <h1 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">Welcome {user?.firstName} {user?.lastName} </h1>
-     
-     <div className="text-center mt-10">
-     <Link href="/mini-zine">
-      <Button className="btn btn-primary">Create minizine</Button>
-              </Link>
-     </div>
+      <h1 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+        Welcome {user?.firstName} {user?.lastName}{" "}
+      </h1>
+
+      <div className="text-center mt-10">
+        <Link href="/mini-zine">
+          <Button className="btn btn-primary">Create Mini Zine</Button>
+        </Link>
+      </div>
     </div>
   );
 }
